aa150b6481ea9d87e0657bf55baef5e905d3710e
Jeff Hammer <jeffrey.hammer2019@gmail.com>
2023-02-08 17:14:13 -0600

    question prompts added

--- index.js
+++ index.js
@@ -8,43 +8,67 @@ const questions = [];
     // Title
     {
       type: 'input',
+      message: 'What would you like to name your ReadMe?',
       title: 'title',
-      message: 'What would you like to name your ReadMe'
     },
     // Description
     {
+      type: 'input',
+      message: 'Give us a description.',
+      name: 'description'
+    },
+    // table of contents type:
+    {
+      type: 'input',
+      message: 'Tell us what you would like in your Table of Contents.',
+      name: 'table of contents'
+    },
+    // installation type: input
+    {
+      type: 'input',
+      message: 'How to Install.',
+      name: 'install'
+    },
+    // usage type: input
+    {
+      type: 'input',
+      message: 'How to Use.',
+      name: 'usage'
+    },
+    // license type: list
+    {
+      type: 'list',
+      name: 'license',
+      message: 'What license would you like to use?',
+      choices: ['MIT', 'GNU General Public License v3.0', 'Apache License 2.0', 'The Unlicense' ]
 
     },
-    // table of contents
+    // contributing type: input
     {
-
+      type: 'input',
+      message: 'List all Contributors',
+      name: 'contributors'
     },
-    // installation
+    // test type: input
     {
-
-    },
-    // usage
-    {
-
-    },
-    // license
-    {
-
-    },
-    // contributing
-    {
-
-    },
-    // test
-    {
-
+      type: 'input',
+      message: 'How to Test.',
+      name: 'test'
     },
     // questions
     {
-
+      type: 'input',
+      message: 'Questions?',
+      name: 'questions'
     },
   ])
-  // .then
+  .then((response) =>{
+
+    console.log(response);
+
+    fs.writeFile('response.txt', JSON.stringify (response, null,), (err) =>  err?console.log(err):console.log('Success'))
+    
+});
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
